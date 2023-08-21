aa0324a4d50468f3936401d7f7d176a413e3bc1d
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-08-22 02:42:04 +0330

    Add Hand-detector in code

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from cvzone.FaceDetectionModule import FaceDetector
 from cvzone.HandTrackingModule import HandDetector
 import cv2 as cv
 
-
 # Create values:
-cap = cv.VideoCapture(1) # Change video capture
+cap = cv.VideoCapture(0) # Change video capture
 
+# detections:
 Face_Detector = FaceDetector(minDetectionCon=10) # Change facedetector options
+Hand_Detector = HandDetector(detectionCon=0.5, maxHands=2) # Change handdetector options 
 
 # Show attributes
 while True:
     success, image = cap.read() # Read camera video
-    Faces, image = Face_Detector.findFaces(image) # Show faces
+    Faces, img = Face_Detector.findFaces(image) # Find Faces
+    Hands, image = Hand_Detector.findHands(image) # Find Hands
 
-    cv.imshow("Robot vision", Faces) # Show camera video
+    cv.imshow("Robot vision", Faces) # Show (Faces on) camera video
+    cv.imshow("Robot vision", image) # Show (Hands on) camera video
     cv.waitKey(1) # Change waitkey
