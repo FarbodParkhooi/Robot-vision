21a4dcd8b92da1bce0fc36b330aaec6ab9822037
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-08-22 02:26:57 +0330

    Create main.py and use face-detection code

---
+++ main.py
@@ -0,0 +1,17 @@
+# import librarys:
+from cvzone.FaceDetectionModule import FaceDetector
+from cvzone.HandTrackingModule import HandDetector
+import cv2 as cv
+
+
+# Create values:
+cap = cv.VideoCapture(1) # Change video capture
+
+Face_Detector = FaceDetector(minDetectionCon=10) # Change facedetector options
+
+while True:
+    success, image = cap.read() # Read camera video
+    Faces, image = Face_Detector.findFaces(image)
+
+    cv.imshow("Robot vision", image)
+    cv.waitKey(1)
