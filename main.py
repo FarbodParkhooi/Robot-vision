11568ce3e55682be2468bdecf0987ecce83dc883
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-08-22 11:29:47 +0330

    Add fullbody detector in code

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ Hand_Detector = HandDetector(detectionCon=0.5, maxHands=2) # Change handdetector
 
 # Cascades:
 eye_cascade = cv.CascadeClassifier("haarcascade_eye.xml") # Read haarcascade_eye.xml
-smile_cascade = cv.CascadeClassifier("haarcascade_smile.xml")# Read haarcascade_eye.xml
+smile_cascade = cv.CascadeClassifier("haarcascade_smile.xml") # Read haarcascade_smile.xml
+fullbody_cascade = cv.CascadeClassifier("haarcascade_fullbody.xml") # Read haarcascade_fullbody.xml
 
 # Show attributes
 while True:
@@ -20,12 +21,15 @@ while True:
     Faces, img = Face_Detector.findFaces(image) # Find Faces
     Hands, image = Hand_Detector.findHands(image) # Find Hands
     eyes = eye_cascade.detectMultiScale(Faces) # Find eyes
+    smiles = smile_cascade.detectMultiScale(Faces, 1.8, 20) # Find smiles
+    fullbody = fullbody_cascade.detectMultiScale(image, scaleFactor=1.1) # Find bodys
+    # Rectangle eyes, smiles and bodys:
     for (ex, ey, ew, eh) in eyes: 
         cv.rectangle(image, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2) # rectangle eyes
-
-    smiles = smile_cascade.detectMultiScale(Faces, 1.8, 20) # Find smiles
     for (ex, ey, ew, eh) in smiles: 
         cv.rectangle(image, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2) # rectangle smiles
+    for (ex, ey, ew, eh) in fullbody: 
+        cv.rectangle(image, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2) # rectangle bodys
 
     cv.imshow("Robot vision", Faces) # Show (Faces on) camera video
     cv.imshow("Robot vision", image) # Show (Hands on) camera video
