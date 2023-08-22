2f5d659ef488329767bfc7b132c52a6f6ec17d84
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-08-22 10:02:47 +0330

    Add eye detector in code

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ cap = cv.VideoCapture(1) # Change video capture
 Face_Detector = FaceDetector(minDetectionCon=10) # Change facedetector options
 Hand_Detector = HandDetector(detectionCon=0.5, maxHands=2) # Change handdetector options 
 
+# Cascades:
+eye_cascade = cv.CascadeClassifier("haarcascade_eye.xml") # Read haarcascade_eye.xml
+
 # Show attributes
 while True:
     success, image = cap.read() # Read camera video
     Faces, img = Face_Detector.findFaces(image) # Find Faces
     Hands, image = Hand_Detector.findHands(image) # Find Hands
+    eyes = eye_cascade.detectMultiScale(Faces) # Find eyes
+    for (ex, ey, ew, eh) in eyes: 
+        cv.rectangle(image, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2) # rectangle eyes
 
     cv.imshow("Robot vision", Faces) # Show (Faces on) camera video
     cv.imshow("Robot vision", image) # Show (Hands on) camera video
